aa48a9cbd25dc91d2d1f2e8b54a586536babff58
jawadhub <jawad.dp@gmail.com>
2021-06-13 14:01:02 +0600

    dependacy added

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,23 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import Comment from '../Comment/Comment';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Comment from "../Comment/Comment";
 
 const PostDetail = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const [post, setPost] = useState({});
   const [comments, setComments] = useState([]);
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => res.json())
       .then((data) => setPost(data));
-  }, [])
+  }, [id]);
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
       .then((res) => res.json())
       .then((data) => setComments(data));
-  }, [])
-
-
+  }, [id]);
 
   return (
     <div>
@@ -26,7 +24,6 @@ const PostDetail = () => {
       <p>User posted : {post.body}</p>
       {comments.map((comment) => (
         <Comment comment={comment}></Comment>
-        
       ))}
     </div>
   );
